d9aef6111b3b741fb54634d71174b40372b5aa97
Stenio <steniolima91@outlook.com>
2023-03-16 13:53:39 -0300

    feat: add mui@5

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material"
 import { Routes, Route, Navigate } from "react-router-dom"
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/inicio" element={<p>Página Inicial</p>} />
+      <Route path="/inicio" element={<Button>TESTE</Button>} />
 
       <Route path="*" element={<Navigate to="/inicio" />} />
     </Routes>
